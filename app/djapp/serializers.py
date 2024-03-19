ba5566204c02ff8cf2be6564b3c70ac364a8a086
adam-ocheri <adamotchery@gmail.com>
2024-03-19 17:55:18 +0200

    View functions and CRUD setup - WIP

--- app/djapp/serializers.py
+++ app/djapp/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import ConversationItem
+from .models import ConversationItem, CallPart
 
 class ConversationItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = ConversationItem
+        fields = ['text', 'speaker', 'search_vector', 'timestamp', 'call_part']
+
+class CallPartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CallPart
         fields = ['text', 'speaker', 'search_vector', 'timestamp', 'call_part']
